8c133841f4256bcfe76962e12aed99f5f200941f
umeshgiriplay <umeshgiriplay@gmail.com>
2019-07-15 13:11:19 +0545

    finish setting up oauth

--- src/main/java/edu/sunway/pustak/oauth/AuthServerConfig.java
+++ src/main/java/edu/sunway/pustak/oauth/AuthServerConfig.java
@@ -1,12 +1,14 @@
 package edu.sunway.pustak.oauth;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 @Configuration
 @EnableAuthorizationServer
@@ -21,8 +23,11 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
 
-    public AuthServerConfig(PasswordEncoder passwordEncoder) {
+    private final AuthenticationManager authenticationManager;
+
+    public AuthServerConfig(PasswordEncoder passwordEncoder, @Qualifier("authenticationManagerBean") AuthenticationManager authenticationManager) {
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
@@ -33,11 +38,17 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     }
 
     @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints){
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+
+    @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient(ClientID)
                 .secret(passwordEncoder.encode(ClientSecret))
-                .authorizedGrantTypes("authorization_code")
+                .authorizedGrantTypes("authorization_code","CLIENT_DETAILS","password", "refresh_token")
                 .scopes("user_info")
                 .autoApprove(true)
                 .redirectUris(RedirectURLs);
